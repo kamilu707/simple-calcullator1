c1b852b54964fdda634337800928f333133177f7
kamilu707 <86446142+kamilu707@users.noreply.github.com>
2021-06-26 16:38:25 +0100

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,25 @@
+function calcule() {
+    var Nb1 = document.f1.nb1.value
+    var Nb2 = document.f1.nb2.value
+    var Op = document.f1.op.value
+    var R1 = document.f1.r1
+
+    switch (op) {
+        case "+":
+            document.f1.r1.value = Nb1 + Nb2;
+            break;
+
+        case "-":
+            document.f1.r1.value = Nb1 + Nb2;
+            break;
+
+        case "*":
+            document.f1.r1.value = Nb1 + Nb2;
+            break;
+
+        case "/":
+            document.f1.r1.value = Nb1 + Nb2;
+            break;
+    }
+
+}
